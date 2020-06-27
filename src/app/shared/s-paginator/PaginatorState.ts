e1e6d70a21e80203e72d8ca9768334cae6e9697f
snehalp29 <snehalpatkar@Gmail.com>
2020-06-27 12:50:21 -0400

    fixed paginator

---
+++ src/app/shared/s-paginator/PaginatorState.ts
@@ -0,0 +1,7 @@
+export interface PaginatorState {
+  first: number;
+  rows: number;
+  page: number;
+  pageCount: number;
+  totalRecords?: number;
+}
--- src/app/shared/s-paginator/s-paginator.component.ts
+++ src/app/shared/s-paginator/s-paginator.component.ts
@@ -1,91 +1,201 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
-
-export interface PageChange {
-  first: number;
-  rows: number;
-  page: number;
-  pageCount: number;
-}
+import { Component, OnInit, EventEmitter, Input, Output, SimpleChanges, OnChanges } from '@angular/core';
+import { PaginatorState } from './PaginatorState';
 
 @Component({
   selector: 'app-s-paginator',
   templateUrl: './s-paginator.component.html',
   styleUrls: ['./s-paginator.component.scss'],
 })
-export class SPaginatorComponent implements OnInit {
+export class SPaginatorComponent implements OnInit, OnChanges {
   /**
    * Number of total records.
    */
-  @Input()
-  totalRecords: number;
+  @Input() totalRecords = 0;
 
   /**
-   *Data count to display per page.
+   * Data count to display per page.
    */
-  @Input()
-  rows: number = 10;
+  @Input() rows = 10;
+
+  @Output() PageChanged: EventEmitter<PaginatorState> = new EventEmitter<PaginatorState>();
 
   /**
-   *Array of integer values to display inside rows per page dropdown. Exp; [10,20,30]
+   * Number of page links to display.
    */
-  @Input()
-  rowsPerPageOptions: number[];
+  @Input() pageLinkSize = 5;
+  currentPage = 0;
+  pageCount = 0;
 
-  @Output()
-  onPageChange: EventEmitter<PageChange> = new EventEmitter<PageChange>();
+  @Input() first = 0;
 
-  /**
-   *Number of page links to display.
-   *
-   */
-  @Input()
-  pageLinkSize = 5;
+  paginatorState: PaginatorState = { first: 0, rows: 0, page: 0, pageCount: 0, totalRecords: 0 };
 
-  currentPage;
+  pageLinks: number[] = [];
 
-  private _pageLinkArray;
+  rowsPerPageItems: number[] = [];
 
-  public get PageLinkArray(): number[] {
-    return this._pageLinkArray;
-  }
-
-  private _firstPage = 1;
 
   constructor() {
-    this.setInitialValues();
-    let isIE11 = !!window.MSInputMethodContext;
-    console.log('isIE', isIE11);
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.updatePaginatorState();
+  }
 
-  setInitialValues() {
-    this.currentPage = 1;
-    this._pageLinkArray = [1, 2, 3, 4, 5];
+  ngOnChanges(simpleChange: SimpleChanges) {
+    if (simpleChange.totalRecords) {
+      this.updatePageLinks();
+      this.updatePaginatorState();
+      this.updateFirst();
+    }
+
+    if (simpleChange.first) {
+      this.first = simpleChange.first.currentValue;
+      this.updatePageLinks();
+      this.updatePaginatorState();
+    }
+
+    if (simpleChange.rows) {
+      this.updatePageLinks();
+      this.updatePaginatorState();
+    }
+
+
+  }
+
+
+
+  updatePageLinks() {
+    this.pageLinks = [];
+    const boundaries = this.calculatePageLinkBoundaries();
+    const start: number = boundaries[0];
+    const end = boundaries[1];
+
+    for (let i = start; i <= end; i++) {
+      this.pageLinks.push(i + 1);
+    }
+  }
+
+  calculatePageLinkBoundaries(): number[] {
+    const numberOfPages = this.getPageCount();
+    const visiblePages = Math.min(this.pageLinkSize, numberOfPages);
+
+    // calculate range, keep current in middle if necessary
+    let start = Math.max(0, Math.ceil(this.getPage() - ((visiblePages) / 2)));
+    const end = Math.min(numberOfPages - 1, start + visiblePages - 1);
+
+    // check when approaching to last page
+    const delta = this.pageLinkSize - (end - start + 1);
+    start = Math.max(0, start - delta);
+
+    return [start, end];
+  }
+
+  updateFirst() {
+    const page = this.getPage();
+    if (page > 0 && this.totalRecords && (this.first >= this.totalRecords)) {
+      Promise.resolve(null).then(() => this.changePage(page - 1));
+    }
+  }
+
+  changePage(p: number) {
+    const pc = this.getPageCount();
+
+    if (p >= 0 && p < pc) {
+      this.first = this.rows * p;
+      const state: PaginatorState = {
+        page: p,
+        first: this.first,
+        rows: this.rows,
+        pageCount: pc
+      };
+      this.updatePageLinks();
+
+      this.PageChanged.emit(state);
+      this.updatePaginatorState();
+    }
+  }
+
+
+  onPageLinkClick(event: Event, page: number) {
+    this.changePage(page);
+    event.preventDefault();
+  }
+
+
+  updatePaginatorState() {
+    this.paginatorState = {
+      page: this.getPage(),
+      pageCount: this.getPageCount(),
+      rows: this.rows,
+      first: this.first,
+      totalRecords: this.totalRecords
+    };
+  }
+
+  isFirstPage() {
+    return this.getPage() === 0;
+  }
+
+  isLastPage() {
+    return this.getPage() === this.getPageCount() - 1;
+  }
+  getPageCount() {
+    return Math.ceil(this.totalRecords / this.rows) || 1;
+  }
+  getPage(): number {
+    return Math.floor(this.first / this.rows);
+  }
+
+  changePageToFirst(event: Event) {
+    if (!this.isFirstPage()) {
+      this.changePage(0);
+    }
+
+    event.preventDefault();
+  }
+
+  changePageToPrev(event: Event) {
+    this.changePage(this.getPage() - 1);
+    event.preventDefault();
+  }
+
+  changePageToNext(event: Event) {
+    this.changePage(this.getPage() + 1);
+    event.preventDefault();
+  }
+
+  changePageToLast(event: Event) {
+    if (!this.isLastPage()) {
+      this.changePage(this.getPageCount() - 1);
+    }
+
+    event.preventDefault();
   }
 
   pageChanged(clickedPage: number) {
     console.log(clickedPage);
 
     this.updatePageLinkArray(
-      this.PageLinkArray,
+      this.pageLinks,
       clickedPage,
       this.pageLinkSize
     );
 
     this.currentPage = clickedPage;
 
-    let pageChanged: PageChange = {
-      first: 1,
+    const pageChanged: PaginatorState = {
+      first: this.first,
       rows: this.rows,
       page: clickedPage,
-      pageCount: 1,
+      pageCount: this.pageCount,
     };
 
-    this.onPageChange.next(pageChanged);
+    this.PageChanged.next(pageChanged);
   }
 
-  updatePageLinkArray(pageLinkArray: number[], currentPage, Size) {
+  updatePageLinkArray(pageLinkArray: number[], currentPage: number, Size: number) {
     pageLinkArray.length = 0;
 
     for (let i = currentPage; i < currentPage + Size; i++) {
@@ -94,16 +204,6 @@ export class SPaginatorComponent implements OnInit {
   }
 
   updateRowSize(newRowValue: number) {
-    this.rows = newRowValue;
-    console.log(this.rows);
-
-    let pageChanged: PageChange = {
-      first: 1,
-      rows: this.rows,
-      page: this.currentPage,
-      pageCount: 1,
-    };
-
-    this.onPageChange.next(pageChanged);
+    this.changePage(this.getPage());
   }
 }
